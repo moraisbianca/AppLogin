d1694e4be4c094b2e1a2af7fb214ac209b107760
moraisbianca <biancacamilamorais@gmail.com>
2022-08-17 15:51:40 -0300

    First Commit

---
+++ AppLogin/AppLogin/App.xaml.cs
@@ -0,0 +1,51 @@
+﻿using AppLogin.Model;
+using AppLogin.View;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppLogin
+{
+    public partial class App : Application
+    {   
+        
+        public List<DadosUsuario> list_usuarios = new List<DadosUsuario>
+        {
+            new DadosUsuario()
+            {
+                Email = "aluno@etec",
+                Nome = "Aluno",
+                Senha = "123"
+            },
+
+            new DadosUsuario()
+            {
+                Email = "professor@etec",
+                Nome = "Professor",
+                Senha = "456"
+            }
+        };
+
+        public App()
+        {
+            InitializeComponent();
+
+                MainPage = new Protegida();
+          
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+
+    }
+}
